c67c867a6df275dbbabf4f98d8a4b15dc24ea8f8
Yury Vashchyla <yury.vashchyla@ya.ru>
2025-11-05 23:35:49 +0100

    - Replace `UnicastProcessor` with `Sinks.Many` in `DelayedBatchExecutor` to leverage Reactor's updated API for better performance and maintainability. - Remove unused `reactor-core.version` property and update dependency versions (`reactor-core`, `junit`, `tinylog`). - Add `versions-maven-plugin` to manage dependency versions and exclude pre-release versions.

--- src/main/java/com/github/victormpcmun/delayedbatchexecutor/DelayedBatchExecutor.java
+++ src/main/java/com/github/victormpcmun/delayedbatchexecutor/DelayedBatchExecutor.java
@@ -1,8 +1,12 @@
 package com.github.victormpcmun.delayedbatchexecutor;
 
-import static java.util.stream.Collectors.toList;
-
 import com.github.victormpcmun.delayedbatchexecutor.callback.BatchAsyncCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
+import reactor.core.scheduler.Schedulers;
+
 import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
@@ -14,11 +18,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import reactor.core.publisher.Mono;
-import reactor.core.publisher.UnicastProcessor;
-import reactor.core.scheduler.Schedulers;
+
+import static java.util.stream.Collectors.toList;
 
 public class DelayedBatchExecutor<A, Z> {
 
@@ -32,10 +33,10 @@ public class DelayedBatchExecutor<A, Z> {
   private final int maxSize;
   private final ExecutorService executorService;
   private final int bufferQueueSize;
-  private final UnicastProcessor<Tuple<A, Z>> source;
+  private final Sinks.Many<Tuple<A, Z>> source;
 
   public DelayedBatchExecutor(Duration duration, int maxSize, ExecutorService executorService, int bufferQueueSize,
-      BatchAsyncCallback<Z, A> batchCallBack) {
+                              BatchAsyncCallback<Z, A> batchCallBack) {
     validateConfigParameters(duration, maxSize, bufferQueueSize);
     this.maxSize = maxSize;
     this.duration = duration;
@@ -46,59 +47,57 @@ public class DelayedBatchExecutor<A, Z> {
   }
 
   public static <Z, A> DelayedBatchExecutor<A, Z> create(
-      Duration duration,
-      int size,
-      BatchAsyncCallback<Z, A> callBack) {
+    Duration duration,
+    int size,
+    BatchAsyncCallback<Z, A> callBack) {
     final ExecutorService executorService = Executors.newFixedThreadPool(DEFAULT_FIXED_THREAD_POOL_COUNTER);
     return new DelayedBatchExecutor<>(duration, size, executorService, DEFAULT_BUFFER_QUEUE_SIZE, callBack);
   }
 
   public static <Z, A> DelayedBatchExecutor<A, Z> create(
-      Duration duration,
-      int size,
-      ExecutorService executorService,
-      int bufferQueueSize,
-      BatchAsyncCallback<Z, A> batchCallback) {
+    Duration duration,
+    int size,
+    ExecutorService executorService,
+    int bufferQueueSize,
+    BatchAsyncCallback<Z, A> batchCallback) {
     return new DelayedBatchExecutor<>(duration, size, executorService, bufferQueueSize, batchCallback);
   }
 
   private void validateConfigParameters(Duration duration, int maxSize, int bufferQueueSize) {
     boolean sizeValidation = maxSize >= 1;
     boolean durationValidation = duration != null
-        && duration.toMillis() >= MIN_TIME_WINDOW_TIME_IN_MILLISECONDS
-        && duration.toMillis() <= MAX_TIME_WINDOW_TIME_IN_MILLISECONDS;
+      && duration.toMillis() >= MIN_TIME_WINDOW_TIME_IN_MILLISECONDS
+      && duration.toMillis() <= MAX_TIME_WINDOW_TIME_IN_MILLISECONDS;
     boolean bufferQueueSizeValidation = (bufferQueueSize >= 1);
     if (!sizeValidation || !durationValidation || !bufferQueueSizeValidation) {
       throw new RuntimeException("Illegal configuration parameters");
     }
   }
 
-  private UnicastProcessor<Tuple<A, Z>> createProcessor(Duration duration, int maxSize, int bufferQueueSize) {
-    // => https://github.com/reactor/reactor-core/issues/469#issuecomment-286040390
+  private Sinks.Many<Tuple<A, Z>> createProcessor(Duration duration, int maxSize, int bufferQueueSize) {
     Queue<Tuple<A, Z>> blockingQueue = new ArrayBlockingQueue<>(bufferQueueSize);
-    UnicastProcessor<Tuple<A, Z>> newSource = UnicastProcessor.create(blockingQueue);
-    newSource.publish()
-        .autoConnect()
-        .bufferTimeout(maxSize, duration)
-        .subscribe(this::invokeBatchCallbackAndContinue);
+    Sinks.Many<Tuple<A, Z>> newSource = Sinks.many().unicast().onBackpressureBuffer(blockingQueue);
+    newSource.asFlux()
+      .bufferTimeout(maxSize, duration)
+      .subscribe(this::invokeBatchCallbackAndContinue);
     return newSource;
   }
 
   private void invokeBatchCallbackAndContinue(List<Tuple<A, Z>> tuples) {
     final List<A> args = tuples.stream().map(Tuple::getArg).collect(toList());
     batchCallBack.apply(args)
-        .doOnError(e -> log.error("invokeBatchCallbackAndContinue: " + e.getMessage(), e))
-        .defaultIfEmpty(Collections.emptyMap())
-        .subscribeOn(Schedulers.fromExecutorService(executorService))
-        .subscribe(rawResultList -> {
-          for (Tuple<A, Z> tuple : tuples) {
-            tuple.getResult().complete(rawResultList.get(tuple.getArg()));
-          }
-        }, e -> {
-          for (Tuple<A, Z> tuple : tuples) {
-            tuple.getResult().completeExceptionally(e);
-          }
-        });
+      .doOnError(e -> log.error("invokeBatchCallbackAndContinue: " + e.getMessage(), e))
+      .defaultIfEmpty(Collections.emptyMap())
+      .subscribeOn(Schedulers.fromExecutorService(executorService))
+      .subscribe(rawResultList -> {
+        for (Tuple<A, Z> tuple : tuples) {
+          tuple.getResult().complete(rawResultList.get(tuple.getArg()));
+        }
+      }, e -> {
+        for (Tuple<A, Z> tuple : tuples) {
+          tuple.getResult().completeExceptionally(e);
+        }
+      });
   }
 
   public Z execute(A arg1) {
@@ -129,13 +128,15 @@ public class DelayedBatchExecutor<A, Z> {
 
   private CompletableFuture<Z> enlistTuple(A arg) {
     final Tuple<A, Z> param = new Tuple<>(arg);
-    source.onNext(param);
+    source.emitNext(param, (signalType, emitResult) ->
+      emitResult == Sinks.EmitResult.FAIL_NON_SERIALIZED
+    );
     return param.getResult();
   }
 
   @Override
   public String toString() {
     return "DelayedBatchExecutor "
-        + "{duration=" + duration.toMillis() + ", size=" + maxSize + ", bufferQueueSize=" + bufferQueueSize + "}";
+      + "{duration=" + duration.toMillis() + ", size=" + maxSize + ", bufferQueueSize=" + bufferQueueSize + "}";
   }
 }
